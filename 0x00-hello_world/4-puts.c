11e02c46872cfab5d4af85eb6536411386c83712
MicoBledsoe <MicoMods@gmail.com>
2022-09-15 08:24:42 -0700

    fdj

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
- * Include
- * Programming
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 puts ("\"Programming is like building a multilingual puzzle");
