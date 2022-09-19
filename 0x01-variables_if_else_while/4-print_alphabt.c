8519f667d6148aea9af4925237bb726445716165
MicoBledsoe <MicoMods@gmail.com>
2022-09-19 09:39:47 -0700

    3

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - Prints the alphabet without q and e.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+		char var[24] = "abcdfghijklmnoprstuvwxyz";
+		int i;
+
+		for ( i=0; i < 24; i++);
+		{
+			putchar(var[i]);
+		}
+			putchar('\n');
+			return (0);i
