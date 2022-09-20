aee31e17df17457721d5ea19364bb2779ef3b151
MicoBledsoe <MicoMods@gmail.com>
2022-09-19 19:04:07 -0700

    djd

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<time.h>
+#include<string.h>
+
+/**
+* main - TEXT
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{       
+char alph[26] = "zyxwvutsrqponmlkjihgfedcba";
+int x;
+
+for ( x = 0; x < 26; x++)
+putchar(alph[x]);
+putchar('\n');
+return(0);
+}     
