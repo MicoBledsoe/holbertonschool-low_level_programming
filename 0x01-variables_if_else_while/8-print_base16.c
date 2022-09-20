b2330162b52348a4951136b7f192e89b6acbbcaf
MicoBledsoe <MicoMods@gmail.com>
2022-09-19 19:18:55 -0700

    dsjd

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<time.h>
+#include<string.h>
+
+/**
+* main - TEXT
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{       
+ char alp[16] = "0123456789abcdef";
+ int y;
+
+for (y = 0; y < 16; y++)
+putchar(alp[y]);
+putchar('\n')
+return (0);
+}
