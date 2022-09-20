e5904bb96fa5ff444aa46895c18038ebba960d15
MicoBledsoe <MicoMods@gmail.com>
2022-09-19 18:39:58 -0700

    dsss

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,14 +1,16 @@
-#include <stdlib.h>
-#include<string.h>
+#include<stdio.h>
 #include<time.h>
+#include<string.h>
 
 /**
- * main - printing numbers from 0 to 9
- *
- * Return: Always 0(Success)
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
 int main(void)
+
 {
 printf("0123456789\n");
-return(0);
+return (0);
 }
