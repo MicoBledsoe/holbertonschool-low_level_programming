38664d8e107d280500ab61d30f66dc81b692e27a
MicoBledsoe <MicoMods@gmail.com>
2022-09-19 18:50:12 -0700

    djdj

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -10,9 +10,9 @@
 
 int main(void)
 {   
-	int i = '0';
-	for ( i = '0'; i <= '9'; i++);
-	putchar(i);
-	putchar('\n');
-	return (0);
+int i = '0';
+for ( i = '0'; i <= '9'; i++);
+putchar(i);
+putchar('\n');
+return (0);
 }
