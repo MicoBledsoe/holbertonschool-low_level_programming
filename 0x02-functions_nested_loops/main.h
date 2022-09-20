8dc620987b09c282c5774665ba683c399217e8ae
MicoBledsoe <MicoMods@gmail.com>
2022-09-20 07:59:37 -0700

    Sdhs

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,12 @@
 #ifndef _MAIN_H
 #define _MAIN_H
-#include <unist.d>
+#include <unistd.h>
+
 int main(void);
-/** Prototype files
+/** prototype file for last project */
+int _putchar(char c);
 void print_alphabet(void);
+int _islower(int c);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
