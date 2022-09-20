26eefe59730783dae778e0460f95a4c054f9ad41
MicoBledsoe <MicoMods@gmail.com>
2022-09-19 18:48:53 -0700

    djd

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<time.h>
+#include<string.h>
+
+/**
+* main - TEXT
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{   
+	int i = '0';
+	for ( i = '0'; i <= '9'; i++);
+	putchar(i);
+	putchar('\n');
+	return (0);
+}
