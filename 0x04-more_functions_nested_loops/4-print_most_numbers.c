c16ff6e155b294c92def3d0b82a08fdad53c2dc9
MicoBledsoe <MicoMods@gmail.com>
2022-09-23 07:40:21 -0700

    dfh

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - upper case characters
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+void print_most_numbers(void)
+{
+int u;
+for (u = '0'; u < '9'; u++)
+{
+if (u != '2' && u != '4')
+_putchar(u);
+}
+_putchar('\n');
+}
