75bac43ff4ab1240edd3a8f8de4c48d00c4fd00b
MicoBledsoe <MicoMods@gmail.com>
2022-09-23 08:32:20 -0700

    fhdsf

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - upper case characters
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+void print_line(int n)
+{
+int p;
+for (p = 0; p < n; p++)
+{
+_putchar(95);
+}
+_putchar('\n');
+}
+}
