8485058453d2f5b3c13859c7defad9332f758404
MicoBledsoe <MicoMods@gmail.com>
2022-09-23 07:12:31 -0700

    dj

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_number  - print 0 to 9
+ *
+ * Return: no return
+ *
+ */
+
+void print_numbers(void)
+}
+int y;
+for (y = '0'; y <= '9', y++)
+{
+_putchar(y);
+}
+_putchar('\n');
+}
