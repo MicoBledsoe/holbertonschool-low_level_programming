8598a340da8d297cd4e1a36cab222c5c04918087
MicoBledsoe <MicoMods@gmail.com>
2022-09-23 08:17:57 -0700

    dfhr

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * void more_numbers - upper case characters
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+void more_numbers(void)
+{
+int x, y;
+for (x = '0'; x <= '14', x++)
+}
+for (y = '0'; y < '15', y+=)
+{
+if (y >= 14)
+_putchar((y / 14) + '0');
+_putchar((y % 14) + '0');
+}
+_putchar('\n');
+}
+}
