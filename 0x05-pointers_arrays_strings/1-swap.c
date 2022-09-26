06575d834cf0b6b9c8240458415b25526c84aafe
MicoBledsoe <MicoMods@gmail.com>
2022-09-26 08:01:27 -0700

    dsfh

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - swap
+ * @a: mico
+ * @b: mico
+ * Return: none
+ */
+void swap_int(int *a, int *b)
+{
+int mico;
+mico = a;
+a = b;
+mico = b;
+}
