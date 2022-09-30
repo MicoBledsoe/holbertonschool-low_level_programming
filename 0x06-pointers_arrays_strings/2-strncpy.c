7ca97f26fdf901a10a9813ca3a529b7d93666f73
MicoBledsoe <MicoMods@gmail.com>
2022-09-30 07:45:52 -0700

    sdfjds

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,13 +13,13 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-{
-	int k;
+	{
+	int i;
 
-	for (k = 0; k < n && src[k] != '\0'; k++)
-		dest[k] = src[k];
-	for ( ; k < n; k++)
-		dest[k] = '\0';
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
 
 	return (dest);
-} 
+}
