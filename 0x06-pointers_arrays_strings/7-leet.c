892cd238f16fe008f7de504b973cde405616a870
MicoBledsoe <MicoMods@gmail.com>
2022-09-30 08:53:22 -0700

    sdg

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337 (leet)
+ * @s: string to convert
+ * Return: pointer to s
+ */
+char *leet(char *s)
+{
+	int i;
+	int j;
+
+	char letters[] = "aAeEoOtTlL";
+	char replace[] = "4433007711";
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (letters[j] != '\0')
+		{
+			if (s[i] == letters[j])
+				s[i] = replace[j];
+			j++;
+		}
+		i++;
+	}
+
+	return (s);
+}
