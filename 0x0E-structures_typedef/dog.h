af2831a4dbc9fd428a8915eac2e79420d2752222
MicoBledsoe <MicoMods@gmail.com>
2022-10-25 10:40:02 -0700

    f

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,28 @@
+#ifndef DOG_H
+#define DOG_H
+#include <stdio.h>
+#include "dog.h"
+int _putchar(char c);
+
+/**
+ * struct dog - structure with elements
+ *@name: first element
+ *@age: second element
+ *@owner: third element
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
