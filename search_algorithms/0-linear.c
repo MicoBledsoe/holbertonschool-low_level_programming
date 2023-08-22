2ee3225d6baa4404cc2c9446cde07bd1a42b9290
MicoBledsoe <micomods@gmail.com>
2023-08-22 02:50:35 -0500

    fix

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -7,7 +7,7 @@
  * @value: The value to search for.
  *
  * Return: The first index where value is located, or -1 if value
- *         is not present in array or if array is NULL.
+ * is not present in array or if array is NULL.
  */
 int linear_search(int *array, size_t size, int value)
 {
